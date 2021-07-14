445e2d0aed621a5ccc0eec76609b27b394a8f373
Jodu555 <Jodu505@gmail.com>
2021-07-14 13:13:02 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # MonitoringSystem-Client
+
+Wireframe
+======
+
+[x] List to Display the single Servers maybe in groups!
+[x] Seperation How to split the page in the different zones!
+[ ] Server Information design!
+[ ] Charts What kind of Chart to use on what data!
---
+++ wireframe/list/script.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('[data-collapse]').forEach(element => {
+    const span = element.getElementsByTagName('span')[0];
+    const target = document.getElementById(element.getAttribute("data-collapse"));
+    // console.log(element);
+    // console.log(target);
+    Array.prototype.slice.call(target.getElementsByTagName('li')).forEach(element => {
+        element.classList.add('collapsible-entity')
+    });
+
+    element.classList.add('collapsible');
+    if (typeof element.getAttribute('expand') !== 'string') {
+        target.style.display = 'none';
+    } else {
+        element.classList.add('expanded');
+    }
+
+    // target
+
+    span.addEventListener('click', () => {
+        element.classList.toggle('expanded');
+        if (target.style.display === 'none') {
+            target.style.display = '';
+        } else {
+            target.style.display = 'none';
+        }
+    });
+
+});
