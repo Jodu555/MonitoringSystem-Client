d8bc3d05db4e92fea4023c8d4763c338b9a7d6c4
Jodu555 <Jodu505@gmail.com>
2021-09-16 16:48:46 +0200

    Complete client is basically Setted up

--- test/index.js
+++ test/index.js
@@ -26,13 +26,29 @@ socket.on('change', ({ server, data }) => {
     console.log('Change', server, data);
     data.forEach(dat => {
         if (dat.type == PERSISTENT_DATA) {
-
+            document.querySelector('#uptime').innerText = secondsToTimeString(dat.uptime);
         }
         if (dat.type == CHANGE_DATA) {
-
+            const { cpu_usage, used_memory, max_memory } = dat;
+            document.querySelector('#cpu').innerText = cpu_usage + '% CPU'
+            document.querySelector('#mem').innerText = used_memory + 'GB / ' + max_memory + 'GB';
         }
     });
 })
+
+function secondsToTimeString(seconds) {
+    let days = Math.floor(seconds / 60 / 60 / 24);
+    seconds = seconds - days * 60 * 60 * 24;
+    let hours = Math.floor(seconds / 60 / 60);
+    seconds = seconds - hours * 60 * 60;
+    let minutes = Math.floor(seconds / 60);
+    seconds = seconds - minutes * 60;
+    const output = '' + (days > 0 ? (days > 9 ? days : '0' + days) + ':' : '')
+        + (hours > 9 ? hours : '0' + hours)
+        + ':' + (minutes > 9 ? minutes : '0' + minutes)
+        + ':' + (seconds > 9 ? seconds : '0' + seconds);
+    return output;
+}
 
 socket.on('connect', () => {
     socket.emit('type', { type: 'client' });
